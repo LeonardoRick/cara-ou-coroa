991b935ed9d397d4a272f4d1f7e19c03a1b9997e
Leonardo Rick <leonardo@lanytecnologia.com.br>
2020-06-11 22:56:42 -0300

    finaliza implementacao do código

--- app/src/main/java/com/example/cara_ou_coroa/MainActivity.java
+++ app/src/main/java/com/example/cara_ou_coroa/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.cara_ou_coroa;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button jogar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        jogar = findViewById(R.id.jogar);
+        jogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int random = randomZeroOrOne();
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class); // Declarando intenção de navegação para a ResultadoActivity
+                intent.putExtra("moeda", random); // Enviando valor da moeda para a ResultadoActivity
+                startActivity(intent); // Navegando para a ResultadoActivity
+            }
+        });
+    }
+
+
+    /**
+    * @return um int com o valor 0 ou 1
+    * */
+    public int randomZeroOrOne() {
+        return new Random().nextInt(2); // número aleatório [0 ou 1]
     }
 
 }
--- app/src/main/java/com/example/cara_ou_coroa/ResultadoActivity.java
+++ app/src/main/java/com/example/cara_ou_coroa/ResultadoActivity.java
@@ -2,13 +2,41 @@ package com.example.cara_ou_coroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
 
+    Button voltar;
+    ImageView imagemMoeda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imagemMoeda = findViewById(R.id.imagemoeda);
+
+        // Configurando o botão voltar para a MainActivity (Encerrando a activity atual
+        Button voltar = findViewById(R.id.voltar);
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Encerra a activity atual e exibe a anterior na pilha
+            }
+        });
+
+        // Recuperando as informações da MainActivity
+        Bundle data = getIntent().getExtras();
+        int random = data.getInt("moeda");
+
+        // Configurando exibição da imagem correta
+        if(random == 0) { // cara
+            imagemMoeda.setImageResource(R.drawable.moeda_cara);
+        } else { // coroa
+            imagemMoeda.setImageResource(R.drawable.moeda_coroa);
+        }
     }
 }
